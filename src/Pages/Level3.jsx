7c65003a997fd3e3b332ec64e99d4a87e47caa01
Shivansh-25 <89247605+Shivansh-25@users.noreply.github.com>
2024-06-21 22:13:53 +0530

    Version 1.03: AI FIXES

--- src/Pages/Level3.jsx
+++ src/Pages/Level3.jsx
@@ -84,18 +84,16 @@ const Level3 = () => {
     diet,
     qualification,
     fieldOfStudy,
-    feedback,
     fullName,
-    email,
   ) => {
     setLoading(true);
     const chatResponse = await client.chat({
-      model: "mistral-large-latest",
+      model: "mistral-large-2402",
       response_format: { type: "json_object" },
       messages: [
         {
           role: "user",
-          content: `Give me 5 questions for a survey on the topic of ${topic} in JSON format in the form of an array with one question at every index please make sure to give the annswer only in json format there should be no ther information, I want the object to contain just the question with the type of "question": "__the question__" nothing else in the object and I want the last element of the array to be a sarcastic summary and comment on the data ${fullName} is the name of the user, if he interested in Technology use the programming language and experience and eignore everything else / if interested in health comment on the diet and exercise frequency and ignore everything else/ if he is interested in education comment only on field of study and qualification and ignore everything else. ${topic} is the topic he is interested in, ${language} is the programming language he uses if he is interested in Technology otherwise it is blank, ${experience} is the amount of experience he has in the field of tech, ${exercise} is the exercise frequency of the user if he is interested in health, ${diet} is the diet preference if interested in health , ${qualification} is his max qualificiation if interested in education, ${fieldOfStudy} is his field of study if interested in education. Try to make thu summary around 100 words long and try to insult the person first but compliment him in the end. Only include and comment on the field he is intersted in`,
+          content: `Give me 5 questions for a survey on the topic of ${topic} in JSON format in the form of an array with one question at every index please make sure to give the annswer only in json format there should be no ther information, I want the object to contain just the question with the type of "question": "__the question__" nothing else in the object and I want the last element of the array to be a sarcastic summary and comment on the data ${fullName} is the name of the user, if he interested in Technology use the programming language and experience and eignore everything else / if interested in health comment on the diet and exercise frequency and ignore everything else/ if he is interested in education comment only on field of study and qualification and ignore everything else. ${topic} is the topic he is interested in, ${language} is the programming language he uses if he is interested in Technology otherwise it is blank, ${experience} is the amount of experience he has in the field of tech, ${exercise} is the exercise frequency of the user if he is interested in health, ${diet} is the diet preference if interested in health , ${qualification} is his max qualificiation if interested in education, ${fieldOfStudy} is his field of study if interested in education. Try to make thu summary around 100 words long and try to insult the person first but compliment him in the end. Only include and comment on the field he is intersted in. MAKE SURE THAT EVEN THE SUMMARY FOLLOWS THE SAME JSON FORMAT AS QUESTIONS "question": "__the summary__"" `,
         },
       ],
     });
@@ -113,9 +111,7 @@ const Level3 = () => {
       values.diet,
       values.qualification,
       values.fieldOfStudy,
-      values.feedback,
       values.fullName,
-      values.email,
     );
     navigate("/additional-questions", { state: { additionalQuestions } });
   };
